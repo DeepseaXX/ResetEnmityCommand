25e6138af64a80cffbe20f3c44b794b1ff1f8add
k-kz <68086259+k-kz@users.noreply.github.com>
2022-11-05 02:18:01 +0000

    API7 Update + Cleanup
    Single target command (`/resetenmity`) working
    Still need to figure out what's going on with `/resetenmityall`

--- Plugin.cs
+++ Plugin.cs
@@ -22,58 +22,56 @@ namespace ResetEnmityCommand
 {
 	public class Plugin : IDalamudPlugin
 	{
-		[PluginService] public static DalamudPluginInterface DalamudPluginInterface { get; private set; }
 		[PluginService] public static TargetManager TargetManager { get; private set; }
-		//[PluginService] public static ObjectTable ObjectTable { get; private set; }
 		[PluginService] public static GameGui GameGui { get; private set; }
 		[PluginService] public static CommandManager CommandManager { get; private set; }
 		[PluginService] public static SigScanner SigScanner { get; private set; }
 
 		private delegate long ExecuteCommandDele(int id, int a1, int a2, int a3, int a4);
-		private ExecuteCommandDele ExecuteCommand;
+		private readonly ExecuteCommandDele ExecuteCommand;
 
 		public void Dispose()
 		{
-			CommandManager.RemoveHandler("/resetenmity");
-			CommandManager.RemoveHandler("/resetenmityall");
+            _ = CommandManager.RemoveHandler("/resetenmity");
+            _ = CommandManager.RemoveHandler("/resetenmityall");
 		}
 
 		public unsafe Plugin()
 		{
-			var scanText = SigScanner.ScanText("E8 ?? ?? ?? ?? 8D 43 0A");
+            IntPtr scanText = SigScanner.ScanText("E8 ?? ?? ?? ?? 8D 43 0A");
 			ExecuteCommand = Marshal.GetDelegateForFunctionPointer<ExecuteCommandDele>(scanText);
 			PluginLog.Debug($"{nameof(ExecuteCommand)} +{(long)scanText - (long)Process.GetCurrentProcess().MainModule.BaseAddress:X} ");
 
-			CommandManager.AddHandler("/resetenmity", new CommandInfo(ResetTarget) { HelpMessage = "Reset target dummy's enmity." });
-			CommandManager.AddHandler("/resetenmityall", new CommandInfo(ResetAll) { HelpMessage = "Reset the enmity of all dummies." });
+            _ = CommandManager.AddHandler("/resetenmity", new CommandInfo(ResetTarget) { HelpMessage = "Reset current target dummy's enmity." });
+            _ = CommandManager.AddHandler("/resetenmityall", new CommandInfo(ResetAll) { HelpMessage = "Reset the enmity of all dummies." });
 
 			void ResetEnmity(int objectId)
 			{
 				PluginLog.Information($"resetting enmity {objectId:X}");
-				ExecuteCommand(0x140, objectId, 0, 0, 0);
+                _ = ExecuteCommand(0x140, objectId, 0, 0, 0);
 			}
 
 			void ResetTarget(string s, string s1)
 			{
-				var target = TargetManager.Target;
+                GameObject target = TargetManager.Target;
 				if (target is Character { NameId: 541 }) ResetEnmity((int)target.ObjectId);
 			}
 
 			void ResetAll(string s, string s1)
 			{
-				var addonByName = GameGui.GetAddonByName("_EnemyList", 1);
-				if (addonByName != IntPtr.Zero)
-				{
-					var addon = (AddonEnemyList*)addonByName;
-					var numArray = FFXIVClientStructs.FFXIV.Client.System.Framework.Framework.Instance()->GetUiModule()->RaptureAtkModule.AtkModule.AtkArrayDataHolder.NumberArrays[19];
+                IntPtr addonByName = GameGui.GetAddonByName("_EnemyList", 1);
+				if (addonByName == IntPtr.Zero)
+					return;
 
-					for (var i = 0; i < addon->EnemyCount; i++)
-					{
-						var enemyObjectId = numArray->IntArray[8 + i * 6];
-						var enemyChara = CharacterManager.Instance()->LookupBattleCharaByObjectId(enemyObjectId);
-						if (enemyChara is null) continue;
-						if (enemyChara->Character.NameID == 541) ResetEnmity(enemyObjectId);
-					}
+                AddonEnemyList* addon = (AddonEnemyList*)addonByName;
+                FFXIVClientStructs.FFXIV.Component.GUI.NumberArrayData* numArray = FFXIVClientStructs.FFXIV.Client.System.Framework.Framework.Instance()->GetUiModule()->GetRaptureAtkModule()->AtkModule.AtkArrayDataHolder.NumberArrays[19];
+
+				for (int i = 0; i < addon->EnemyCount; i++)
+				{
+                    int enemyObjectId = numArray->IntArray[8 + (i * 6)];
+                    FFXIVClientStructs.FFXIV.Client.Game.Character.BattleChara* enemyChara = CharacterManager.Instance()->LookupBattleCharaByObjectId(enemyObjectId);
+					if (enemyChara is null) continue;
+					if (enemyChara->Character.NameID == 541) ResetEnmity(enemyObjectId);
 				}
 			}
 		}
